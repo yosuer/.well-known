a1c40247b10379229d4e40eb7d2616ad3e07de42
Iguanafix <Iguanafix@Iguanadev002>
2016-11-04 12:55:04 -0300

    sliders

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -6,7 +6,7 @@ var index = (function () {
     }
 
     function initMain() {
-        // makes sure the whole site is loaded
+// makes sure the whole site is loaded
         $(window).on("load", loadPreloader);
         // Touch screen device
         if ("ontouchstart" in window) {
@@ -14,20 +14,21 @@ var index = (function () {
             isTouch = true;
         }
         new WOW().init();
-
     }
 
     function initScroll() {
         $('#fullPage').fullpage({
             anchors: ['primera', 'bienvenidos', 'comofunciona', 'destacados', 'tequeremos'],
-            menu: '#menu',
-            verticalCentered: true,
+            verticalCentered: false,
             css3: true,
             normalScrollElements: '#section3',
             responsiveWidth: 320,
             scrollOverflow: true,
             slidesNavigation: true,
-            scrollingSpeed: 800
+            scrollingSpeed: 800,
+            afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+                $("#iguana-telefono-contenido").attr("src", "img/paso" + slideIndex + ".gif");
+            }
         });
     }
 
@@ -40,7 +41,6 @@ var index = (function () {
         init: init
     };
 })();
-
 $(document).ready((function () {
     index.init();
 }));
