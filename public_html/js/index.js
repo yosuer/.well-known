bc4af3e83322050a63086d9050b967e6ac15abcf
Iguanafix <Iguanafix@Iguanadev002>
2016-11-07 18:48:09 -0300

    arreglos

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,45 +1,21 @@
 var index = (function () {
 
-    var configFullPage = {
-        anchors: ['primera', 'bienvenidos', 'comofunciona', 'destacados', 'tequeremos'],
-        verticalCentered: true,
-        css3: false,
-        responsiveWidth: 320,
-        scrollOverflow: true,
-        slidesNavigation: true,
-        scrollingSpeed: 800,
-        recordHistory: false,
-        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
-            $(".iguana-telefono-contenido").fadeOut(300, function () {
-                $(".iguana-telefono-contenido").attr("src", "img/paso" + nextSlideIndex + ".gif");
-                $(".iguana-telefono-contenido").fadeIn(200);
-            });
-            console.log('anchorLink: ' + anchorLink + ', index: ' + index + ', slideIndex: ' + slideIndex +
-                    ', direction: ' + direction + ', nextSlideIndex: ' + nextSlideIndex);
-        },
-        onLeave: function (index, nextIndex, direction) {
-            if (index === 1 && nextIndex === 2) {
-                $('#section0 .wow').css('opacity', 0.7);
-                console.log(index + '  ' + nextIndex + '  ' + direction);
-            }
-            if (index === 2 && nextIndex === 1) {
-                $('#section0 .wow').css('opacity', 1);
-                console.log(index + '  ' + nextIndex + '  ' + direction);
-            }
-        },
-        afterRender: function () {
-            console.log('after render');
-
-            $('.nav-fillpath .next').on('click', function (e) {
-                e.preventDefault();
-                $.fn.fullpage.moveSlideRight();
-            });
-            $('.nav-fillpath .prev').on('click', function (e) {
-                e.preventDefault();
-                $.fn.fullpage.moveSlideLeft();
-            });
+    function isMobile() {
+        if (navigator.userAgent.match(/Android/i)
+                || navigator.userAgent.match(/webOS/i)
+                || navigator.userAgent.match(/iPhone/i)
+                || navigator.userAgent.match(/iPad/i)
+                || navigator.userAgent.match(/iPod/i)
+                || navigator.userAgent.match(/BlackBerry/i)
+                || navigator.userAgent.match(/Windows Phone/i)
+                ) {
+            return true;
+        } else {
+            return false;
         }
-    };
+    }
+
+
 
     function initMain() {
         $(window).on("load", function () {
@@ -49,13 +25,53 @@ var index = (function () {
         new WOW().init();
     }
 
-    function initScroll() {
+    function initFullPage() {
+
+        var configFullPage = {
+            anchors: ['primera', 'bienvenidos', 'comofunciona', 'destacados', 'tequeremos'],
+            verticalCentered: true,
+            css3: isMobile(),
+            responsiveWidth: 320,
+            scrollOverflow: true,
+            slidesNavigation: true,
+            scrollingSpeed: 800,
+            recordHistory: false,
+            onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
+                $(".iguana-telefono-contenido").fadeOut(300, function () {
+                    $(".iguana-telefono-contenido").attr("src", "img/paso" + nextSlideIndex + ".gif");
+                    $(".iguana-telefono-contenido").fadeIn(200);
+                });
+                console.log('anchorLink: ' + anchorLink + ', index: ' + index + ', slideIndex: ' + slideIndex +
+                        ', direction: ' + direction + ', nextSlideIndex: ' + nextSlideIndex);
+            },
+            onLeave: function (index, nextIndex, direction) {
+                if (index === 1 && nextIndex === 2) {
+                    $('#section0 .wow').css('opacity', 0.7);
+                    console.log(index + '  ' + nextIndex + '  ' + direction);
+                }
+                if (index === 2 && nextIndex === 1) {
+                    $('#section0 .wow').css('opacity', 1);
+                    console.log(index + '  ' + nextIndex + '  ' + direction);
+                }
+            },
+            afterRender: function () {
+                $('.nav-fillpath .next').on('click', function (e) {
+                    e.preventDefault();
+                    $.fn.fullpage.moveSlideRight();
+                });
+                $('.nav-fillpath .prev').on('click', function (e) {
+                    e.preventDefault();
+                    $.fn.fullpage.moveSlideLeft();
+                });
+            }
+        };
+
         $('#fullPage').fullpage(configFullPage);
     }
 
     function init() {
         initMain();
-        initScroll();
+        initFullPage();
     }
 
     return {
