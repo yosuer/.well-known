c923d00358c8022e177b2445951336151391c56b
Iguanafix <Iguanafix@Iguanadev002>
2016-11-07 11:13:35 -0300

    prueba parallax

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,34 +1,30 @@
 var index = (function () {
 
+    var configFullPage = {
+        anchors: ['primera', 'bienvenidos', 'comofunciona', 'destacados', 'tequeremos'],
+        verticalCentered: true,
+        css3: false,
+        responsiveWidth: 320,
+        scrollOverflow: true,
+        slidesNavigation: true,
+        scrollingSpeed: 700,
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+            $("#iguana-telefono-contenido").attr("src", "img/paso" + slideIndex + ".gif");
+        }
+    };
+
     function loadPreloader() {
         $("#status").fadeOut(1000);
         $("#preloader").delay(500).fadeOut();
     }
 
     function initMain() {
-// makes sure the whole site is loaded
         $(window).on("load", loadPreloader);
-        // Touch screen device
-        if ("ontouchstart" in window) {
-            document.documentElement.className = document.documentElement.className + " touch";
-            isTouch = true;
-        }
         new WOW().init();
     }
 
     function initScroll() {
-        $('#fullPage').fullpage({
-            anchors: ['primera', 'bienvenidos', 'comofunciona', 'destacados', 'tequeremos'],
-            verticalCentered: true,
-            css3: false,
-            responsiveWidth: 320,
-            scrollOverflow: true,
-            slidesNavigation: true,
-            scrollingSpeed: 800,
-            afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-                $("#iguana-telefono-contenido").attr("src", "img/paso" + slideIndex + ".gif");
-            }
-        });
+        $('#fullPage').fullpage(configFullPage);
     }
 
     function init() {
@@ -40,6 +36,7 @@ var index = (function () {
         init: init
     };
 })();
+
 $(document).ready((function () {
     index.init();
 }));
